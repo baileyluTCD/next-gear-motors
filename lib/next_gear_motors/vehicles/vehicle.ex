61afe39a325160f02f9db36d897dee4b10f325de
Luke Bailey <baileylu@tcd.ie>
2025-08-11 22:53:06 +0100

    Validate that uploads are finished before allowing saving

--- lib/next_gear_motors/vehicles/vehicle.ex
+++ lib/next_gear_motors/vehicles/vehicle.ex
@@ -10,6 +10,7 @@ defmodule NextGearMotors.Vehicles.Vehicle do
   import Ecto.Changeset
 
   alias NextGearMotors.Vehicles.Covers.Cover
+  alias NextGearMotors.Vehicles.Vehicle.UploadsStatusType
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -19,6 +20,7 @@ defmodule NextGearMotors.Vehicles.Vehicle do
     field :price, :string
     field :manufacturer, :string
     field :covers, {:array, Cover.Type}, default: []
+    field :covers_uploads_status, UploadsStatusType, virtual: true, default: :finished
     has_many :reservations, NextGearMotors.Reservations.Reservation
 
     timestamps(type: :utc_datetime)
@@ -26,8 +28,18 @@ defmodule NextGearMotors.Vehicles.Vehicle do
 
   def changeset(vehicle, attrs) do
     vehicle
-    |> cast(attrs, [:name, :covers, :price, :description, :manufacturer])
-    |> validate_required([:name, :covers, :price, :description, :manufacturer])
+    |> cast(attrs, [:name, :covers, :price, :description, :manufacturer, :covers_uploads_status])
+    |> validate_required([
+      :name,
+      :covers,
+      :price,
+      :description,
+      :manufacturer,
+      :covers_uploads_status
+    ])
+    |> validate_inclusion(:covers_uploads_status, [:finished],
+      message: "all image uploads must be finished"
+    )
     |> validate_format(:price, ~r/^€[0-9|,|\.]*$/,
       message: "must be a number in euro - i.e. '€19,999.99'"
     )
